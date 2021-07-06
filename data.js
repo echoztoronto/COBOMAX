876a033006660eddfb0cf16187c0c32039453348
YW Z <32604270+echoztoronto@users.noreply.github.com>
2021-07-06 01:03:11 -0400

    refactor part II

---
+++ data.js
@@ -0,0 +1,16 @@
+// please save all images in jpg format
+// then add the image names into the array
+// example: 1.jpg and random.jpg  ->   [1,'random']
+
+// the slide show on homepage
+const home_slider_img = [0,1,2];
+const home_slider_path = "image/slider/";
+
+// for active programs
+const active_program_img = [];   // program page will only display past posters if this array is empty
+const active_program_path = "image/program/";
+
+// for past programs
+const past_program_img = [6,5,4,3,2,1,0];
+const past_program_path = "image/program/";
+
--- style/general.js
+++ style/general.js
@@ -1,4 +1,6 @@
+// navbar
 let navbar = document.getElementById("navbar");
+if(navbar !== null)
 navbar.innerHTML = 
 `<div class="container-fluid">
     <div class="navbar-header">
@@ -12,16 +14,17 @@ navbar.innerHTML =
         <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav navbar-right">
             <li><a href="index.html#about"><span class="glyphicon glyphicon-info-sign"></span> ABOUT </a></li>
-            <li><a href="achievement.html"><span class="glyphicon glyphicon-globe"></span> ACHIEVEMENT </a></li>
+            <li><a href="showcase.html"><span class="glyphicon glyphicon-globe"></span> SHOWCASE </a></li>
             <li><a href="program.html"><span class="glyphicon glyphicon-education"></span> PROGRAM </a></li>
-            <li><a href="project.html"><span class="glyphicon glyphicon-th-list"></span> PROJECT </a></li>
             <li><a href="index.html#help"><span class="glyphicon glyphicon-thumbs-up"></span> HELP US </a></li>
             <li><a href="index.html#contact"><span class="glyphicon glyphicon-envelope"></span> CONTACT</a></li>
         </ul>
     </div>
 </div>`;
 
+// footer
 let footer = document.getElementById("footer");
+if(footer !== null) 
 footer.innerHTML = 
 `<hr>
     <div class="text-center center-block">
@@ -37,3 +40,77 @@ footer.innerHTML =
         </div>               
     </div>
 <hr>`;
+
+
+// slider
+generate_slider("myCarousel",home_slider_path,home_slider_img);
+if(active_program_img.length !== 0) generate_slider("program-slider",active_program_path,active_program_img);
+else {
+    change_to_past();
+    document.getElementById("program-subtitle").remove();
+}
+
+
+
+
+
+function generate_slider(ID, dir, img_array) {
+    let container = document.getElementById(ID);
+    if(container === null) return;
+
+    let indicator = 
+        `<ol class="carousel-indicators hidden-xs hidden-sm">
+            <li data-target="#${ID}" data-slide-to="0" class="active"></li>`;;
+
+    for(let i = 1; i < img_array.length; i++) {
+        indicator += `<li data-target="#${ID}" data-slide-to="${i}"></li>`;
+    }
+    indicator +=  `</ol>`;
+
+    let slides = 
+        `<div class="carousel-inner">
+            <div class="item active">
+                <a href="${dir}${img_array[0]}.jpg" target="_blank">
+                    <img src="${dir}${img_array[0]}.jpg" alt="1">
+                </a>
+            </div>`;
+    for(let i = 1; i < img_array.length; i++) {
+        slides += 
+            `<div class="item">
+                <a href="${dir}${img_array[i]}.jpg" target="_blank">
+                    <img src="${dir}${img_array[i]}.jpg" alt="${i}">
+                </a>
+            </div>`;
+    }
+    slides +=  `</div>`;
+
+    container.innerHTML = indicator + slides +
+        `<a class="left carousel-control hidden-xs" href="#${ID}" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control hidden-xs" href="#${ID}" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>`;
+
+}
+
+
+function change_to_past(){
+    document.getElementById("program-title").innerHTML = `Past Programs`;
+    document.getElementById("program-subtitle").innerHTML = `View Active Programs`;
+    document.getElementById("program-slider").remove;
+    document.body.innerHTML += `<div id="program-slider" class="carousel slide" data-ride="carousel"> </div>`;
+    generate_slider("program-slider",past_program_path,past_program_img);
+    document.getElementById("program-subtitle").onclick = change_to_active;
+}
+
+function change_to_active(){
+    document.getElementById("program-title").innerHTML = `Active Programs`;
+    document.getElementById("program-subtitle").innerHTML = `View Past Programs`;
+    document.getElementById("program-slider").remove;
+    document.body.innerHTML += `<div id="program-slider" class="carousel slide" data-ride="carousel"> </div>`;
+    generate_slider("program-slider",active_program_path,active_program_img);
+    document.getElementById("program-subtitle").onclick = change_to_past;
+}
